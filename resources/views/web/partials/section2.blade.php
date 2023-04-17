54b65713785f5a70a03cfbc3207135bfb9b4f772
Karolis <karolis@agradaweb.com>
2023-04-17 14:28:05 +0200

    Update. Se ha hecho la sección 2

---
+++ resources/views/web/partials/section2.blade.php
@@ -0,0 +1,37 @@
+<div class="flex justify-center">
+    <p class=" border-b-4 border-green-400 text-lg font-bold text-gray-400 text-center mb-6">A FRIENDLY AND EXPERIENCED</p>
+</div>
+<div class="text-3xl lg:text-4xl xl:text-5xl font-medium text-center mb-16">
+    <p>An Agency of Designers,</p>
+    <p>Developers and Content Makers</p>
+</div>
+<div class="grid grid-cols-1 gap-7 lg:grid-cols-2 xl:grid-cols-4 mx-10 xl:mx-0">
+    <div class="text-center">
+        <svg class="w-36 h-36 mx-auto pb-5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"></path>
+        </svg>
+        <p class="text-2xl font-semibold pb-5">Digital Marketing</p>
+        <p class="text-xl font-normal">Lorem ipsum indolor st amet, cm etctetur adips locing elit, sedi din m st ilmes.</p>
+    </div>
+    <div class="text-center">
+        <svg class="w-36 h-36 mx-auto pb-5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"></path>
+        </svg>
+        <p class="text-2xl font-semibold pb-5">Digital Marketing</p>
+        <p class="text-xl font-normal">Lorem ipsum indolor st amet, cm etctetur adips locing elit, sedi din m st ilmes.</p>
+    </div>
+    <div class="text-center">
+        <svg class="w-36 h-36 mx-auto pb-5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"></path>
+        </svg>
+        <p class="text-2xl font-semibold pb-5">Digital Marketing</p>
+        <p class="text-xl font-normal">Lorem ipsum indolor st amet, cm etctetur adips locing elit, sedi din m st ilmes.</p>
+    </div>
+    <div class="text-center">
+        <svg class="w-36 h-36 mx-auto pb-5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"></path>
+        </svg>
+        <p class="text-2xl font-semibold pb-5">Digital Marketing</p>
+        <p class="text-xl font-normal">Lorem ipsum indolor st amet, cm etctetur adips locing elit, sedi din m st ilmes.</p>
+    </div>
+</div>
